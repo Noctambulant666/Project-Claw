8e18cd62d2380bd92d1549b9a9f1a39e339c16a0
PDuck95 <pduck95@gmail.com>
2018-07-16 16:57:39 +1200

    16.07.02

---
+++ Project Claw/Assets/Scripts/Game/RotatingCollectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCollectable : MonoBehaviour {
+    float speed = 2f;
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime * speed);
+
+    
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            EventManager.TriggerEvent ("coinpickup")
+        }
+    }
+}
